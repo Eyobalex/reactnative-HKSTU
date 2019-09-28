d55cf3765cc395cc0fbecc996c492f2fc8119e1c
Eyobalex <eyobalemayehu007@gmail.com>
2019-09-28 21:16:12 +0300

    a

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#5f7a4b',
+    color: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
